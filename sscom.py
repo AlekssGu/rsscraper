9d0504a4d8a78ab74f7ad27bb6b84f25da086e38
Aleksandrs Gusevs <aleksandrs.gusevs@proofit.lv>
2019-11-30 11:11:00 +0200

    Added Telegram integration

--- sscom.py
+++ sscom.py
@@ -2,6 +2,8 @@ import common
 import categories
 import advertisments
 import spreadsheet
+import telegram
+import system_parameters
 
 def main():
     soup_process()
@@ -10,5 +12,11 @@ def main():
 def soup_process():  
     categories.process()
     advertisments.process()
+
+def notify_via_telegram():
+    my_token = system_parameters.TELEGRAM_TOKEN
+    chat_id = system_parameters.TELEGRAM_CHAT_ID
+    bot = telegram.Bot(token=my_token)
+    bot.sendMessage(chat_id=chat_id, text='Update')
 
 main()
